5a4edf64d5ba32ff4a655c76e77f703ffe117d6c
ethanial1 <camiloeiz00@gmail.com>
2022-02-23 17:48:40 -0600

    App de Tareas con typescript

--- task/src/App.tsx
+++ task/src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ITask {
+    nombre: string,
+    done: boolean
+}
+
+
+function App(): JSX.Element {
+    const [task, setTask] = useState<string>("")
+    const [tasks, setTasks] = useState<ITask[]>([])
+
+    const taskInput = useRef<HTMLInputElement>(null);
+
+    const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+        setTask(e.currentTarget.value)
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        addTask(task)
+        taskInput.current?.focus();
+    }
+
+    const addTask = (nombre: string): void => {
+        setTasks([...tasks, {nombre, done: false}])
+        setTask('')
+    }
+
+    const toggleDoneTask = (index: number): void => {
+        const newTasks: ITask[] = tasks.slice();
+        newTasks[index].done = !newTasks[index].done
+
+        setTasks(newTasks)
+    }
+
+    const removeTask = (index: number): void => {
+        const newTasks: ITask[] = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks)
+    }
+
+    return (
+        <div className='tasks'>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="name" ref={taskInput} value={task} onChange={handleChange}/>
+                <button type="submit">Guardar</button>
+            </form>
+            <ul>
+            {
+                tasks.map((t: ITask, index: number) => (
+                    <li key={index}>
+                        <span>{t.nombre}</span>
+                        <div>
+                            <button onClick={() => toggleDoneTask(index)}>
+                                <span>{t.done ? "done" : "x"}</span>
+                            </button>
+                            <button onClick={() => removeTask(index)}>
+                                <span>delete</span>
+                            </button>
+                        </div>
+                    </li>
+                ))
+            }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
